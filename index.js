29cd899518f0dc054c50ce3f8ca0b6cedf0cd2f8
Dan Vidmar <65089365+RegattaBurrata@users.noreply.github.com>
2023-01-20 14:44:11 -0800

    Add grid button, add clear button, create painting functionality

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
-const boxSize = '16'
+let boxSize = '16'
 const gridContainer = document.querySelector('.gridContainer');
+const chooseGridSize = document.querySelector('.gridSizeButton')
+const clear = document.querySelector('.clearButton');
 
 const createBoxDiv = () => {
     const box = document.createElement('div');
@@ -17,3 +19,36 @@ const createGrid = (size) => {
 }
 
 createGrid(boxSize);
+
+const boxOn = (e) => {
+    console.log(e);
+    e.srcElement.classList.add('boxOn');
+}
+
+const mouseOver = () => {
+    const allBoxes = document.querySelectorAll('.box');
+    allBoxes.forEach((box) => { box.addEventListener('mouseover', boxOn) })
+}
+
+mouseOver();
+
+const clearGridSize = () => {
+    gridContainer.innerHTML = ('');
+}
+
+chooseGridSize.addEventListener('click', function () {
+    boxSize = prompt('Enter the number of rows');
+    clearGridSize();
+    if (boxSize < 100) {
+        createGrid(boxSize);
+        mouseOver();
+    }
+    else {
+        alert('Must be a number below 100!');
+    }
+});
+
+clear.addEventListener('click', function () {
+    const allBoxes = document.querySelectorAll('.box');
+    allBoxes.forEach((box) => { box.classList.remove('boxOn') })
+})
